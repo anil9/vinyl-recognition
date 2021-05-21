dfbd673eb13d9a96e39a3a79b0af07aa3312f80f
andreas <anil9>
2021-05-21 22:50:37 +0200

    Improved CatalogueNumberExtractor

--- src/main/java/org/nilsson/catno/CatalogueNumberExtractor.java
+++ src/main/java/org/nilsson/catno/CatalogueNumberExtractor.java
@@ -5,32 +5,82 @@ import org.apache.log4j.Logger;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class CatalogueNumberExtractor {
     private static final Logger LOG = LogManager.getLogger(CatalogueNumberExtractor.class);
+    public static final String CATALOGUE_REGEX = "([A-Z0-9]+([ \\-][A-Z0-9]+)*){3,}";
+    public static final String CATALOGUE_WHOLE_LINE_REGEX = "^" + CATALOGUE_REGEX + "$";
+    private static Pattern patternForCatalogue = Pattern.compile(CATALOGUE_REGEX);
+    private static Pattern patternForWholeLineCatalogue = Pattern.compile(CATALOGUE_WHOLE_LINE_REGEX);
+    private static Set<String> BLOCKED_WORDS = Set.of("STEREO", "APM", "RPM");
 
     public static String extractCatalogueNumber(String text) {
         LOG.debug(Arrays.stream(text.split(System.lineSeparator())).collect(Collectors.toList()));
 
-        List<String> collect = Arrays.stream(text.split(System.lineSeparator()))
-                .filter(s -> s.matches("[A-Z0-9]+([ \\-][A-Z0-9]+)*"))
-                .filter(s -> s.matches(".*\\d+.*"))
-                .collect(Collectors.toList());
+        List<String> collect = findCatalogueNumberWholeLine(text);
+        collect.addAll(findCatalogueNumberWholeWords(text));
+        collect.addAll(findCatalogueNumberWordsWithinLine(text));
+
         if (collect.isEmpty()) {
             LOG.error("no catalogue number found");
             return null;
         }
         if (collect.size() != 1) {
-            LOG.info("multiple catalogue numbers match, attempting to pick correct: " + collect);
+            LOG.info("multiple catalogue numbers match, attempting to pick correct: {}" + collect);
 
-            return collect.stream()
-                    .filter(s -> s.length() >= 5 && s.length() <= 10)
+            String picked = collect.stream()
+                    .filter(s -> s.length() >= 5 && s.length() <= 14)
                     .findFirst()
                     .orElse(collect.get(0));
+            LOG.info("picked=" + picked);
+            return picked;
         }
         return collect.get(0);
 
+    }
+
+    private static List<String> findCatalogueNumberWordsWithinLine(String text) {
+        return Arrays.stream(text.split(System.lineSeparator()))
+                .map(s -> patternForCatalogue.matcher(s))
+                .filter(Matcher::find)
+                .map(matcher -> matcher.group(0))
+//                .filter(s -> s.matches(CATALOGUE_REGEX))
+                .peek(System.out::println)
+                .filter(s -> s.matches(".*\\d+.*"))
+                .filter(withoutBlockedWords())
+                .filter(s -> s.length() > 2)
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> findCatalogueNumberWholeWords(String text) {
+        return Arrays.stream(text.split(System.lineSeparator()))
+                .flatMap(s -> Arrays.stream(s.split("\\s")))
+                .filter(s -> s.matches(CATALOGUE_REGEX))
+                .filter(s -> s.matches(".*\\d+.*"))
+                .filter(withoutBlockedWords())
+                .filter(s -> s.length() > 2)
+                .collect(Collectors.toList());
+    }
+
+    private static List<String> findCatalogueNumberWholeLine(String text) {
+        return Arrays.stream(text.split(System.lineSeparator()))
+                .map(s -> patternForWholeLineCatalogue.matcher(s))
+                .filter(Matcher::find)
+                .map(matcher -> matcher.group(0))
+                .filter(s -> s.matches(CATALOGUE_REGEX))
+                .filter(s -> s.matches(".*\\d+.*"))
+                .filter(withoutBlockedWords())
+                .filter(s -> s.length() > 2)
+                .collect(Collectors.toList());
+    }
+
+    private static Predicate<String> withoutBlockedWords() {
+        return s -> Arrays.stream(s.split("\\s")).noneMatch(BLOCKED_WORDS::contains);
     }
 
 }
--- src/test/java/org/nilsson/catno/CatalogueNumberExtractorTest.java
+++ src/test/java/org/nilsson/catno/CatalogueNumberExtractorTest.java
@@ -1,5 +1,6 @@
 package org.nilsson.catno;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,24 +12,41 @@ public class CatalogueNumberExtractorTest {
     public void shouldFindCatalogueNumber() {
         String expected = "E 048-35144";
 
-        String catalogueNumber = CatalogueNumberExtractor.extractCatalogueNumber("LETTER\n" +
-                "4E 048-35144\n" +
-                "SO\n" +
-                "\n" +
-                "ZARAH LEANDER rv\n" +
-                "mod Einar Groths orkestafi.T-3\n" +
-                "mod Arno Hulphers”öfkostar: 4, 5\n" +
-                "\n" +
-                "fa\n" +
-                "jary - Runo Moborg)\n" +
-                "\n" +
-                "RT rad felt AA SN [Kr\n" +
-                "Maborg). 4. KVINNOR, KVINNOR, KVINNOR, KVINNOR\n" +
-                "[STATE EN TEESE SDL VALT\n" +
-                "LUTNING RaED\n" +
-                "Gösta Rybranl).\n" +
-                "\n" +
-                " \n");
+        String catalogueNumber = CatalogueNumberExtractor.extractCatalogueNumber("""
+                                        
+                TA
+                           
+                LTT ENEE
+                                
+                4E 048-35144
+                CI
+                                
+                \s
+                                
+                V
+                J
+                                
+                mod Einar Groths orkestafi.T-3
+                G
+                7
+                                
+                ZARAH LEANDER
+                                
+                ELR VETT RENEE
+                                
+                ZY TSE TASSAR TNE FETT MKT Er Tee
+                                
+                Is. Forlin) 2. SKEPP OHOJ (Michaol Jary = Runo Maborg]
+                                
+                CH SÅ DANSAR JAG EN ARDAS (Michael Jary = Rune
+                                
+                Maborg) 4. KVINNOR; KVINNOR, KVI INNOR, KVINNOR
+                [STATE EN TEESE SDL VALT
+                                
+                UTTER NR TG RN
+                Gösta Rybroni)
+
+                """);
 
         assertThat(catalogueNumber).containsPattern("[A-Z]?" + expected + "[A-Z]?");
 
@@ -37,14 +55,111 @@ public class CatalogueNumberExtractorTest {
     @Test
     public void shouldPickCorrectCatalogueNumber() {
         String expected = "31058 I";
-        String catalogueNumber = CatalogueNumberExtractor.extractCatalogueNumber("Sa\n" +
-                "M33\n" +
-                "\n" +
-                "vV\n" +
-                "KATALOG-NR.\n" +
-                "31058 I");
+        String catalogueNumber = CatalogueNumberExtractor.extractCatalogueNumber("""
+                Sa
+                M33
+
+                vV
+                KATALOG-NR.
+                31058 I""");
 
         assertThat(catalogueNumber).isEqualTo(expected);
+
+    }
+
+    @Test
+    public void shouldPickCorrectCatalogueNumber2() {
+        String expected = "2600831";
+        String catalogueNumber = CatalogueNumberExtractor.extractCatalogueNumber("""
+
+                a rpm 2600831
+                ereo (4, 6, 7, 9, 10)
+                no (1-3, 5, 8)
+
+                SIDA A
+                b
+                G 132 Testare HERMAN MÖLLER hälsar välkommen 2. MIN
+                AVET. Tal sg. [CVert Taube) EVERT TALIAE = äre SKIMRANDE VAR ALDRIG
+                AVET 141 (E ND DU VÄ LSI RAGANCIA 3.25 (Evoig ORG
+                YENLREKNLTAUDE 5. LAND DU VÄLSIGNARE Alt rk
+                JUSSI BJÖRLING 6. FARVÄL a > Heep. Hellberg) ZARAIN
+                LEANDER 7. VISSI DARTE Puccini) BIRGIT NI
+                Karl Goshor OCH KARUSELLEN 1.20. frin el GT Öst
+                Kari Gerhard : Gösta Stenberg) KARL Gr 3jl/ HAGA""");
+
+        assertThat(catalogueNumber).isEqualTo(expected);
+
+    }
+
+    @Ignore(value = "CatalogueNumberExtractor is not advanced enough to perfectly extract correct")
+    @Test
+    public void shouldPickCorrectCatalogueNumber3() {
+        String expected = "PL 40164";
+        String catalogueNumber = CatalogueNumberExtractor.extractCatalogueNumber("""
+                Vw
+                                
+                sidal Stereo
+                                
+                Hela världen behöver ha en sång
+                Halleluja | 5:
+                | Ske fö L PL 40164
+                1; HELA VÄRLDEN BEHUVER HA EN SÄNG
+                - Moreno - Olle: Bergman)
+                rr KOshrar « Olle Bergman)
+                                
+                Kyssande vind
+                Vals till Marie ;
+                Dansen går på Svinsta skär SA SN
+                Gläds åt var morgon är
+                f
+                ES fena Keen Hjalmar Gullberg)
+                                
+                       
+                [ISAE 25001
+                4. VALS TILL MARIE.
+                [Adam Mirchell = Beng! Hoslum)
+                5. DANSEN OAR PÅ SVINSTA SKAR
+                (Gideon Wahlberg)
+                6. GLADS AT VAR MORGON
+                [CarStevens + E Forieon =
+                iawkey Fronzen)
+                JAN MALMSJO
+                Lars Samuelson med kör och orkester
+                Producent; Carl.Eric Hjelm
+                """);
+
+        assertThat(catalogueNumber)
+                .contains(expected)
+                .hasSizeLessThan(expected.length() + 3);
+
+    }
+
+    @Ignore(value = "CatalogueNumberExtractor is not advanced enough to perfectly extract correct")
+    @Test
+    public void shouldPickCorrectCatalogueNumber4() {
+        String expected = "LBLP 008";
+        String catalogueNumber = CatalogueNumberExtractor.extractCatalogueNumber("""
+                                
+                LBLP 008 A DEM nce
+                2 APM STEREO
+                                
+                LILL LINDFORS
+                "EN LILLsk JUL"
+                                
+                TAI RUTALLON GÅR I DANSEN 223 (Trac. Arr. Peter Ljung)
+                3 MD TUSEN JULELJUS 2.07 (E. Köhler,
+                RÅDER 2.02 (W. Sofva/A. Smedberg) DF
+                (Trad. Ar.
+                                
+                '. Peter Ljung)
+                $- STILLA NATT 2.55 (F. Gruberg.
+                tad. Arr. Peter Ljung)
+                          
+                """);
+
+        assertThat(catalogueNumber)
+                .contains(expected)
+                .hasSizeLessThan(expected.length() + 3);
 
     }
 }
