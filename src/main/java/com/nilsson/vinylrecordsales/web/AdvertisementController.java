61c81519bfb3cb743d656f78c88f1a822ac888e2
andreas <anil9>
2021-10-31 15:06:50 +0100

    Fix bug where image urls were popped even though no ad was created

--- src/main/java/com/nilsson/vinylrecordsales/web/AdvertisementController.java
+++ src/main/java/com/nilsson/vinylrecordsales/web/AdvertisementController.java
@@ -1,7 +1,6 @@
 package com.nilsson.vinylrecordsales.web;
 
 import com.nilsson.vinylrecordsales.AdvertisementService;
-import com.nilsson.vinylrecordsales.domain.ProductId;
 import com.nilsson.vinylrecordsales.image.ImageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,46 +20,46 @@ import static java.util.Objects.requireNonNull;
 @Controller
 public class AdvertisementController {
 
-	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-	private static final int NUMBER_OF_IMAGES_PER_AD = 5;
-	private static final int MAX_CONCURRENT = 10;
-	private final AdvertisementService advertisementService;
-	private final ImageService imageService;
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final int NUMBER_OF_IMAGES_PER_AD = 5;
+    private static final int MAX_CONCURRENT = 10;
+    private final AdvertisementService advertisementService;
+    private final ImageService imageService;
 
-	public AdvertisementController(AdvertisementService advertisementService, ImageService imageService) {
-		this.advertisementService = requireNonNull(advertisementService, "advertisementService");
-		this.imageService = requireNonNull(imageService, "imageService");
-	}
+    public AdvertisementController(AdvertisementService advertisementService, ImageService imageService) {
+        this.advertisementService = requireNonNull(advertisementService, "advertisementService");
+        this.imageService = requireNonNull(imageService, "imageService");
+    }
 
-	@GetMapping("/record")
-	public String recordForm(Model model) {
-		model.addAttribute("recordFinder", new RecordFinder());
-		if (!imageService.haveStoredURLs()) {
-			LOG.warn("Haven't uploaded any images yet");
-		}
-		return "record-finder";
-	}
+    @GetMapping("/record")
+    public String recordForm(Model model) {
+        model.addAttribute("recordFinder", new RecordFinder());
+        if (!imageService.haveStoredURLs()) {
+            LOG.warn("Haven't uploaded any images yet");
+        }
+        return "record-finder";
+    }
 
-	@PostMapping("/record")
-	public String recordSubmit(@ModelAttribute RecordFinder recordFinder, Model model) {
-		model.addAttribute("recordFinder", new RecordFinder());
-		LOG.info("POSTing {}", recordFinder);
-		if (recordFinder.getCatalogueId().isBlank()) {
-			return "redirect:/record";
+    @PostMapping("/record")
+    public String createAd(@ModelAttribute RecordFinder recordFinder, Model model) {
+        model.addAttribute("recordFinder", new RecordFinder());
+        LOG.info("POSTing {}", recordFinder);
+        if (recordFinder.getCatalogueId().isBlank()) {
+            return "redirect:/record";
 
-		}
-		if (!imageService.haveStoredURLs()) {
-			throw new IllegalStateException("Cannot create ad if no image url has been stored");
-		}
-		Flux<URL> imageUrls = Flux.range(0, NUMBER_OF_IMAGES_PER_AD)
-				.flatMapSequential(i -> imageService.pollUrl(), MAX_CONCURRENT);
+        }
+        if (!imageService.haveStoredURLs()) {
+            throw new IllegalStateException("Cannot create ad if no image url has been stored");
+        }
+        Flux<URL> imageUrls = Flux.range(0, NUMBER_OF_IMAGES_PER_AD)
+                .flatMapSequential(i -> imageService.pollUrl(), MAX_CONCURRENT);
 
-		Mono<ProductId> productId = advertisementService.monoCreateAdvertisement(recordFinder.getCatalogueId(),
-				recordFinder.getExtraTitleWords()).log();
+        advertisementService.monoCreateAdvertisement(recordFinder.getCatalogueId(), recordFinder.getExtraTitleWords())
+                .log()
+                .flatMapMany(id -> advertisementService.addImages(Mono.just(id), imageUrls))
+                .subscribe(url -> LOG.info("Stored url on product, url={}", url));
 
-		advertisementService.addImages(productId, imageUrls)
-				.subscribe(url -> LOG.info("Stored url on product, url={}", url));
-		return "redirect:/record";
-	}
+        return "redirect:/record";
+    }
 
 }
---
+++ src/test/java/com/nilsson/vinylrecordsales/web/AdvertisementControllerTest.java
@@ -0,0 +1,78 @@
+package com.nilsson.vinylrecordsales.web;
+
+import com.nilsson.vinylrecordsales.AdvertisementService;
+import com.nilsson.vinylrecordsales.domain.ProductId;
+import com.nilsson.vinylrecordsales.image.ImageService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AdvertisementControllerTest {
+
+    @Mock
+    AdvertisementService advertisementService;
+    @Mock
+    ImageService imageService;
+    @Mock
+    Model model;
+
+    private AdvertisementController advertisementController;
+    private URL url;
+
+    @BeforeEach
+    void setUp() throws MalformedURLException {
+        advertisementController = new AdvertisementController(advertisementService, imageService);
+        url = new URL("https://httpstat.us");
+    }
+
+    @Test
+    void createAdAndAddImages() {
+        //given
+        final RecordFinder recordFinder = new RecordFinder();
+        recordFinder.setCatalogueId("aCatalogueId");
+        when(imageService.haveStoredURLs()).thenReturn(true);
+        final Mono<ProductId> productId = Mono.just(new ProductId(11));
+        when(advertisementService.monoCreateAdvertisement(recordFinder.getCatalogueId(), null)).thenReturn(productId);
+        final Flux<URL> urls = Flux.just(url, url, url, url, url);
+        when(advertisementService.addImages(any(), any())).thenReturn(urls);
+        //when
+        advertisementController.createAd(recordFinder, model);
+        //then
+        verify(imageService).haveStoredURLs();
+        verifyNoMoreInteractions(imageService);
+        verify(advertisementService).monoCreateAdvertisement(recordFinder.getCatalogueId(), null);
+        verify(advertisementService).addImages(any(), any());
+        verifyNoMoreInteractions(advertisementService);
+
+    }
+
+    @Test
+    void dontAddImagesIfAdWasntCreated() {
+        //given
+        final RecordFinder recordFinder = new RecordFinder();
+        recordFinder.setCatalogueId("aCatalogueId");
+        when(imageService.haveStoredURLs()).thenReturn(true);
+        final Mono<ProductId> productId = Mono.empty();
+        when(advertisementService.monoCreateAdvertisement(recordFinder.getCatalogueId(), null)).thenReturn(productId);
+        //when
+        advertisementController.createAd(recordFinder, model);
+        //then
+        verify(imageService).haveStoredURLs();
+        verifyNoMoreInteractions(imageService);
+        verify(advertisementService).monoCreateAdvertisement(recordFinder.getCatalogueId(), null);
+        verifyNoMoreInteractions(advertisementService);
+
+    }
+}
